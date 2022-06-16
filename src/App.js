49ec1e91ce4fc74154613717ff58eb732057d3b5
German <gerfersie@gmail.com>
2022-06-16 13:11:20 +0200

    Para la Sofi UwU 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "antd/dist/antd.css";
+import Login from './components/Login/Login';
+import Profile from './components/Profile/Profile';
+import Products from './components/Products/Products';
+import Header from './components/Header/Header';
+import { UserProvider } from './context/UserContext/UserState';
+import { ProductsProvider } from './context/ProductsContext/ProductsState';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserProvider>
+        <ProductsProvider>
+        <BrowserRouter>
+        <Header />
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </BrowserRouter>
+        </ProductsProvider>
+      </UserProvider>
     </div>
+
   );
 }
 
